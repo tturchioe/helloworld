3d18459b7799354187b6e7f31e4158a3f5bc43c3
Tom Turchioe <tom.turchioe@sap.com>
2021-04-05 16:00:18 -0700

    added code to view.when to find SPL layer

--- geomap_component.js
+++ geomap_component.js
@@ -154,6 +154,11 @@
                     
                     var changedValue = changedProperties["servicelevel"];
                     debug.log( changedValue);
+
+                    // find the SPL sublayer so a query is issued
+                    var svcLyr = webmap.findLayerById( 'NapervilleElectric_MIL1_1724' );
+                    var myLyr = svcLyr.findSublayerById(6);
+                    debug.log( myLyr)
                 });
           }); // end of require()
       } // end of class()
